528ed32a28acd5413f9ee2f53eec4fca282af132
Saketh <sakethrox@gmail.com>
2019-10-21 22:17:42 -0700

    redux base level

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
-import {CREATE_RECIPE, REMOVE_FROM_CALENDAR} from '..actions'
-import { ADD_RECIPE } from '../actions'
+import {ADD_RECIPE, REMOVE_FROM_CALENDAR} from '../actions'
+
 
 
 const initialCalendarState = {
